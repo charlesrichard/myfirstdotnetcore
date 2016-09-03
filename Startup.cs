d267da8d1192e77c0b91d6c04a873ebb8af0ecfa
Chuck Svoboda <crsvoboda@gmail.com>
2016-09-03 14:45:10 -0400

    add extra newline

--- Startup.cs
+++ Startup.cs
@@ -6,7 +6,10 @@ namespace ConsoleApplication {
         public void Configure(IApplicationBuilder app){
             app.Run(context => {
                 //return context.Response.WriteAsync("Hello world");
-                return context.Response.WriteAsync("Yup, this is C#.Net!" + System.Environment.NewLine + System.Environment.StackTrace);
+                return context.Response.WriteAsync("Yup, this is C#.Net!" + 
+                       System.Environment.NewLine + 
+                       System.Environment.NewLine + 
+                       System.Environment.StackTrace);
             });
         }
     }

646d223f0c3605ecc6ae2fc975764367c6df4eed
Chuck Svoboda <crsvoboda@gmail.com>
2016-09-03 14:19:13 -0400

    Create Startup.cs

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+ 
+namespace ConsoleApplication {
+    public class Startup{
+        public void Configure(IApplicationBuilder app){
+            app.Run(context => {
+                return context.Response.WriteAsync("Hello world");
+            });
+        }
+    }
+}
